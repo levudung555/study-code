80d49b9658505d54994b763d5481862afeab83a9
levudung555 <dunglevu321456@gmail.com>
2024-09-14 15:18:19 +0700

    bai tap for

---
+++ baitap13/ConsoleApplication29/ConsoleApplication29/ConsoleApplication29.cpp
@@ -0,0 +1,32 @@
+// ConsoleApplication29.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int coso, somu;
+    long luythua=1 ;
+        
+    cout << "Vui long nhap co so :";
+    cin >> coso;
+    cout << "Vui long nhap so mu :";
+    cin >> somu;
+    for (int i = 1; i <= somu; i++) {
+        luythua = luythua * coso;
+
+    }cout << "-------------------" << endl;
+    cout << coso << " ^ " << somu << " = " << luythua << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
